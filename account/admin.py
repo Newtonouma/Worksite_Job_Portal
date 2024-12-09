ea1b38464892fc5b8aa7c8a3f85aefc4a31c89cc
unknown <armstrongnewton02@gmail.com>
2024-12-09 15:00:38 +0300

    Changes Added Successfully

--- account/admin.py
+++ account/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from resume.models import Resume
 from company.models import Company
+from job.models import Job, JobApplication
+
 
 # Register the model
 admin.site.register(Resume)
 admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(JobApplication)
 
 
